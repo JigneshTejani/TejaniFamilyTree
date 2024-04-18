0cef22331ed65b8c57f96f688cc8fb25e4ba8581
JigneshTejani <jigneshtejani227@gmail.com>
2024-04-18 22:21:23 +0530

    :fire: Build updated for github pages

--- src/tree/index.js
+++ src/tree/index.js
@@ -1,74 +1,12 @@
 import React from 'react';
 import Tree from 'react-d3-tree';
 import {useCenteredTree} from "./useCentered";
-
-// This is a simplified example of an org chart with a depth of 2.
-// Note how deeper levels are defined recursively via the `children` property.
-const orgChart = {
-    name: "Ratanpar Tejani Family Tree", children: [{
-        name: "Naran", children: [{
-            name: "Pancha", children: []
-        }, {name: "Mavji", children: []}, {
-            name: 'Ramji', children: [{
-                name: 'Valji', children: [{
-                    name: 'Pravin', children: [{
-                        name: 'Sarth'
-                    }, {name: 'Manali'}],
-                }, {
-                    name: 'Haresh', children: [{
-                        name: 'Yug'
-                    }, {name: 'Neksha'}],
-                },]
-            }, {
-                name: 'Daya', children: [{
-                    name: 'Nilesh', children: [{
-                        name: 'Priyanshi'
-                    }, {name: 'Jash'}],
-                }, {
-                    name: 'Jagdish', children: [{
-                        name: 'Isha'
-                    }, {name: 'Man'}],
-                },]
-            }, {
-                name: 'Gordhan', children: [{
-                    name: 'Jayesh', children: [{
-                        name: 'Vraj'
-                    }, {name: 'Kriva'}],
-                }, {
-                    name: 'Ashvin', children: [{
-                        name: 'Harvi'
-                    }, {name: 'Shivang'}],
-                },]
-            },]
-        }, {
-            name: "Chhagan", children: [{
-                name: "Madhavji", children: [{name: "Dinesh"}, {name: "Janak"}, {name: "Suresh"}]
-            }]
-        },]
-    }, {
-        name: "Ramji", children: [{
-            name: "Jadav", children: [{
-                name: "Makod", children: [{
-                    name: "Gordhan", children: [{
-                        name: 'Tushar', children: [{
-                            name: 'Dhruvaansh'
-                        }]
-                    }, {name: 'Jignesh'}],
-                }, {
-                    name: "Pravin", children: [{name: 'Rohit', children: [{name: 'Praj'}]}, {name: 'Ridham'}]
-                }, {
-                    name: "Late Bharat", children: [{name: 'Rajan'}, {name: 'Rutul'}]
-                }]
-            }]
-        }, {
-            name: "Jina",
-            children: [{name: 'Mohan', children: [{name: 'Vasharam', children: [{name: "Haresh", children: [{name: "Jenish"}]}, {name: "Raju", children: [{name: "Tilak"}]}]}]}]
-        }]
-    }]
-};
+import treeJson from './tree.json';
+import {stratify} from 'd3-hierarchy';
 
 export default function OrgChartTree() {
     const [dimensions, translate, containerRef] = useCenteredTree();
+    const familyTree = stratify().id((d) => d.name).parentId((d) => d.father)(treeJson);
 
     return (<React.Fragment>
         <div id="treeWrapper" style={{width: '100vw', height: '100vh'}} ref={containerRef}>
@@ -82,37 +20,43 @@ export default function OrgChartTree() {
                 nodeSize={{x: 200, y: 300}}
                 renderCustomNodeElement={(treeData) => <RenderCard data={{...treeData}}/>}
                 initialDepth={6} // for visible first one row in tree
-                data={orgChart}
+                data={familyTree}
             />
         </div>
     </React.Fragment>);
 }
 
 const RenderCard = React.memo(({data: {nodeDatum, toggleNode, foreignObjectProps = {}}}) => {
+    const data = nodeDatum?.data
+    console.log('nodeDatum', data)
+    const id = data?.unique_id
+    const name = data?.name?.split(" ")?.[0]
+    const spouse = data?.spouse?.split(" ")?.[0]
+    const late = data?.late?.trim() === 'y' || data?.late?.trim() === 'Y'
+    const spouse_late = data?.spouse_late?.trim() === 'y' || data?.spouse_late?.trim() === 'Y'
+    const number = data?.number
     return (<React.Fragment>
         <foreignObject
             {...foreignObjectProps}
             width="200"
             height="300"
             x="-100"
-            y="-150"
+            y="-100"
         >
             <div className="card-container" onClick={toggleNode}>
                 <div className="card">
                     <div className="user">
                         <div className='user-avatar'>
                             <img src="https://static.vecteezy.com/system/resources/previews/009/397/835/non_2x/man-avatar-clipart-illustration-free-png.png"/>
-                            <p>{nodeDatum?.name || '-'}</p>
+                            <p>{late ? 'Late. ' : ''}{(id !== '1') ? `${name} Bhai` : data?.name}</p>
                         </div>
 
-                        {/*<div className='user-avatar'>*/}
-                        {/*    <img src="https://static.vecteezy.com/system/resources/thumbnails/018/787/001/small/avatar-job-business-woman-flat-portrait-of-woman-png.png"/>*/}
-                        {/*    <p>{nodeDatum?.name || '-'}</p>*/}
-                        {/*</div>*/}
+                        {spouse && <div className='user-avatar'>
+                            <img src="https://static.vecteezy.com/system/resources/thumbnails/018/787/001/small/avatar-job-business-woman-flat-portrait-of-woman-png.png"/>
+                            <p>{spouse_late ? 'Late. ' : ''}{spouse} Ben</p>
+                        </div>}
                     </div>
-                    <div className="contact">
-                        <p>9574357964</p>
-                    </div>
+                    {number && <div className="contact"><p>{number}</p></div>}
                 </div>
             </div>
         </foreignObject>
