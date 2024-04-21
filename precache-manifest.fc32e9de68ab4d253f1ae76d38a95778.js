ec96b2738b690f0016154d38c2b0a2fada74c858
JigneshTejani <jigneshtejani227@gmail.com>
2024-04-21 14:52:25 +0530

    Updates

---
+++ precache-manifest.fc32e9de68ab4d253f1ae76d38a95778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f92942fe44965184d4eb8ae791de01e",
+    "url": "/TejaniFamilyTree/index.html"
+  },
+  {
+    "revision": "6b42b3fbafbfc9c7e081",
+    "url": "/TejaniFamilyTree/static/css/main.7ec17585.chunk.css"
+  },
+  {
+    "revision": "59a32b15a7ac0c123c8d",
+    "url": "/TejaniFamilyTree/static/js/2.48fedce3.chunk.js"
+  },
+  {
+    "revision": "e26de4f0e0d98f1626b38316e0457409",
+    "url": "/TejaniFamilyTree/static/js/2.48fedce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b42b3fbafbfc9c7e081",
+    "url": "/TejaniFamilyTree/static/js/main.55881dee.chunk.js"
+  },
+  {
+    "revision": "703be729ed2445c9b634",
+    "url": "/TejaniFamilyTree/static/js/runtime-main.f8a8150c.js"
+  }
+]);
